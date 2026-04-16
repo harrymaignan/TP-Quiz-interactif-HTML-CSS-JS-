b3456bfbbba9be741522ac66c8b130a294a0c299
Fernando <fernandofkd.sio@gmail.com>
2026-04-16 16:48:45 +0200

    Ajout du score et vérification des réponses

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
 const question = document.querySelector("#Questions");
 
 const start = document.querySelector("#start");
+const scoreValide= document.querySelector("#Score")
 
 const a = document.querySelector("#Question1");
 const b = document.querySelector("#Question2");
 const c = document.querySelector("#Question3");
 
+let currentindex = 0;
 function commencerquiz() {
-  question.innerHTML = question1[0].question;
-  a.innerHTML = question1[0].answers[0];
-  b.innerHTML = question1[0].answers[1];
-  c.innerHTML = question1[0].answers[2];
+  if (currentindex < question1.length){
+      question.innerHTML = question1[currentindex].question;
+      a.innerHTML = question1[currentindex].answers[0];
+      b.innerHTML = question1[currentindex].answers[1];
+      c.innerHTML = question1[currentindex].answers[2];
+  }
+ 
 }
 
-function reponse(choix) {}
+
+let score=0;
+
+function reponse(choix) {
+  
+  
+
+  if(choix==question1[currentindex].correct){
+    
+    score++
+   
+  }
+  else{
+    score=0
+  }
+  scoreValide.innerHTML = "Score : " + score;
+
+  currentindex++;
+  commencerquiz();
+  
+}
+
+
+
 
 const question1 = [
-  {
+   {
     question: "Quel est le président actuel ?",
     answers: ["Emmanuel Macron", "Cristiano Ronaldo", "François Hollande"],
     correct: 0,
   },
-];
-
-const question2 = [
   {
     question: "Quelle est l'équipe ayant le plus de league des champions",
     answers: ["Fc Milan", "Réal-Madrid", "Fc Barcelone"],
     correct: 1,
   },
-];
-
-const question3 = [
   {
     question: "Qui a gagner la coupe du monde 2022?",
     answers: ["Portugal", "France", "Argentine"],
     correct: 2,
   },
-];
-
-const question4 = [
   {
     question: "Qui a gagner le ballon d'or 2012?",
     answers: ["Neymar", "Messi", "Ronaldo"],
     correct: 1,
   },
-];
-
-const question5 = [
   {
-    question: "Qui  est le champion UFC des moins de 70kg?",
+    question: "Qui est le champion UFC des moins de 70kg?",
     answers: ["Charles Oliveira", "Ilia Topuria", "Arman Tsyarukyan"],
     correct: 1,
-  },
+  }
 ];
 
 start.addEventListener("click", () => commencerquiz());
-a.addEventListener("click", () => reponse());
-b.addEventListener("click", () => reponse());
-c.addEventListener("click", () => reponse());
+a.addEventListener("click", () => reponse(0) );
+b.addEventListener("click", () => reponse(1));
+c.addEventListener("click", () => reponse(2));
