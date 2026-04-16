8b19c3310114a55d2c986081a30ff4eb5ce32536
hrry78z <harrymaignan@gmail.com>
2026-04-16 15:02:31 +0200

    ajouts des questions 3,4,5

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const questions = [
-{
-question: "Quel est le président actuel ?",
-answers: ["Emmanuel Macron", "Cristiano Ronaldo", "François Hollande"],
-correct: 0
-},
-
-{
-question: "Quelle est l'équipe ayant le plus de league des champions",
-answers: ["Fc Milan", "Réal-Madrid", "Fc Barcelone"],
-correct: 1
-}
+const question1 = [
+  {
+    question: "Quel est le président actuel ?",
+    answers: ["Emmanuel Macron", "Cristiano Ronaldo", "François Hollande"],
+    correct: 0,
+  },
 ];
 
+const question2 = [
+  {
+    question: "Quelle est l'équipe ayant le plus de league des champions",
+    answers: ["Fc Milan", "Réal-Madrid", "Fc Barcelone"],
+    correct: 1,
+  },
+];
+
+const question3 = [
+  {
+    question: "Qui a gagner la coupe du monde 2022?",
+    answers: ["Portugal", "France", "Argentine"],
+    correct: 2,
+  },
+];
+
+const question4 = [
+  {
+    question: "Qui a gagner le ballon d'or 2012?",
+    answers: ["Neymar", "Messi", "Ronaldo"],
+    correct: 1,
+  },
+];
+
+const question5 = [
+  {
+    question: "Qui  est le champion UFC des moins de 70kg?",
+    answers: ["Charles Oliveira", "Ilia Topuria", "Arman Tsyarukyan"],
+    correct: 1,
+  },
+];
